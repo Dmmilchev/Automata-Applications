0dc71c1e0fd35f740b1085aa2163ace3efd25c32
Dmmilchev <deo1508@abv.bg>
2025-05-30 17:53:20 +0300

    remvoed

--- build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/deyan/Desktop/Minimal\ Subsequential\ Transducer/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/deyan/Desktop/Minimal\ Subsequential\ Transducer/build/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/deyan/Desktop/Minimal Subsequential Transducer/test
-# Build directory: /home/deyan/Desktop/Minimal Subsequential Transducer/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/deyan/Desktop/Minimal Subsequential Transducer/build/test/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/deyan/Desktop/Minimal Subsequential Transducer/test/CMakeLists.txt;9;add_test;/home/deyan/Desktop/Minimal Subsequential Transducer/test/CMakeLists.txt;0;")
